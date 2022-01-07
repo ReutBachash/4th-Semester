a1aa2f257cf36e2f4c5c8dd5bdd7f11450fc7e64
MNHyper <>
2022-01-07 17:20:11 +0200

    do

---
+++ Assets/script/ZoneOneST.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZoneOneST : MonoBehaviour
+{
+    [SerializeField] Animation Enemy;
+
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
