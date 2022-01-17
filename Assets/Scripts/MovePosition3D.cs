186d950d9e3e8a619cd595f8c10915119e0bab1e
ReutaBanana <Reutaxdd@gmail.com>
2022-01-17 20:16:22 +0200

    reuta fixed moving circle + deleted conditions in CM controller

--- Assets/Scripts/MovePosition3D.cs
+++ Assets/Scripts/MovePosition3D.cs
@@ -5,12 +5,13 @@ using UnityEngine;
 public class MovePosition3D : MonoBehaviour
 {
     [SerializeField] private Camera mainCamera;
+    [SerializeField] private LayerMask mouseColiderLayerMask;
 
     // Update is called once per frame
     void Update()
     {
         Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
-        if(Physics.Raycast(ray,out RaycastHit hit))
+        if(Physics.Raycast(ray,out RaycastHit hit,999f,mouseColiderLayerMask))
         {
             transform.position = hit.point;
         }
