14cc8ae6f8a84cca03882613dbfc4b24a4e4d731
MNHyper <>
2022-02-11 01:27:56 +0200

    Revert "Reuta fixed logic and added point & health"
    This reverts commit e0006f58dc9eb8927e1c110594728845f55df37d.

--- Assets/Scripts/SequenceSystem/AnimationHandler.cs
+++ Assets/Scripts/SequenceSystem/AnimationHandler.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class AnimationHandler : MonoBehaviour
 {
-    [SerializeField] private Animator animator;
+    private Animator animator;
     private void Awake()
     {
         animator = this.gameObject.GetComponent<Animator>();
--- Assets/Scripts/Shoot.cs
+++ Assets/Scripts/Shoot.cs
@@ -7,11 +7,8 @@ public class Shoot : MonoBehaviour
 {
     public float Range = 1000;
     public float Force = 1000;
-    private float _hitRange=2000;
     [SerializeField] private Camera mainCamera;
     [SerializeField] private LayerMask mouseColiderLayerMask;
-
-    public event Action<EnemyIdentity> onEnemyDeath;
 
 
     // Update is called once per frame
@@ -24,12 +21,11 @@ public class Shoot : MonoBehaviour
     void RayShoot()
     {
         Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
-        if (Physics.Raycast(ray, out RaycastHit hit, _hitRange, mouseColiderLayerMask))
+        if (Physics.Raycast(ray, out RaycastHit hit, 200000f, mouseColiderLayerMask))
         {
             Debug.DrawLine(ray.origin, hit.point,Color.blue);
             if(hit.transform.CompareTag("Enemy"))
             {
-                onEnemyDeath.Invoke(hit.transform.GetComponentInParent<EnemyIdentity>());
                 hit.transform.parent.GetComponentInChildren<Animator>().SetTrigger("Death");
                 hit.transform.parent.GetComponentInParent<Animator>().SetTrigger("Death");
                  Debug.Log("Hit");
