d589be5ad47aa04bc00690c5a7c1bc905cedb8cc
ReutaBanana <Reutaxdd@gmail.com>
2022-03-15 22:15:37 +0200

    Reuta added trigger for zone 2&3

--- Assets/Scripts/TriggerHandler.cs
+++ Assets/Scripts/TriggerHandler.cs
@@ -19,6 +19,12 @@ public class TriggerHandler : MonoBehaviour
        if(zone1Cleared)
         {
             this.gameObject.GetComponent<Animator>().SetTrigger("Zone1Cleared");
+        }else if(zone2Cleared)
+        {
+            this.gameObject.GetComponent<Animator>().SetTrigger("Zone2Cleared");
+        }else if(zone3Cleared)
+        {
+            this.gameObject.GetComponent<Animator>().SetTrigger("Zone3Cleared");
         }
     }
 }
