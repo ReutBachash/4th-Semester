4c88cd232cae6a9c60292ba2c372612890b0b75b
MNHyper <>
2022-03-19 14:27:48 +0200

    to fix?

---
+++ Assets/Scripts/ZoneMananger.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZoneMananger : MonoBehaviour
+{
+    [SerializeField] GameObject zones;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    private void EnableNextZone(int nextZoneNumber)
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
