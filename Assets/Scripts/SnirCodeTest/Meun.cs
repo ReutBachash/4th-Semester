bbf7c4c0778932f6c45de0ce67215360a6138fdf
MNHyper <>
2022-04-01 18:28:58 +0300

    Menu Scenes

---
+++ Assets/Scripts/SnirCodeTest/Meun.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Meun : MonoBehaviour
+{
+    public void LoadNext()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void GameOuit()
+    {
+        Application.Quit();
+        Debug.Log("Quit");
+    }
+}
