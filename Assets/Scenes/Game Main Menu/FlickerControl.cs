7ae6bd02e8801ca3b76a9508a2cbb5a875f2b1cc
ReutaBanana <Reutaxdd@gmail.com>
2022-04-07 14:46:36 +0300

    added flickering lights

--- Assets/Scenes/Game Main Menu/FlickerControl.cs
+++ Assets/Scenes/Game Main Menu/FlickerControl.cs
@@ -7,6 +7,9 @@ public class FlickerControl : MonoBehaviour
     public bool isFlickering = false;
     public float timeDelay;
 
+    [SerializeField] GameObject plane;
+    [SerializeField] GameObject spotlight;
+
     void Update()
     {
      if (isFlickering == false)
@@ -18,10 +21,12 @@ public class FlickerControl : MonoBehaviour
     IEnumerator FlickeringLight()
     {
         isFlickering = true;
-        this.gameObject.GetComponent<Light>().enabled = false;
+        plane.SetActive(false);
+        spotlight.SetActive(false);
         timeDelay = Random.Range(0.01f, 0.5f);
         yield return new WaitForSeconds(timeDelay);
-        this.gameObject.GetComponent<Light>().enabled = true;
+        plane.SetActive(true);
+        spotlight.SetActive(true);
         timeDelay = Random.Range(0.01f, 0.5f);
         yield return new WaitForSeconds(timeDelay);
         isFlickering = false;
